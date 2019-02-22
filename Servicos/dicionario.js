ab0484bb73c838e08c73fb439df97780e69d1e60
Igor Carlos Almeida <almeidaigorcarlos@gmail.com>
2019-02-22 09:53:43 -0300

    Refatoracao do modulo 'dicionario.js'

--- Servicos/dicionario.js
+++ Servicos/dicionario.js
@@ -1,113 +1,98 @@
-var request = require('request');
-var configuracao = require('./../Arquivos/configuracao');
+const request = require('request');
+const configuracao = require('./../Arquivos/configuracao');
 
-var headers = {
+const headers = {
     'Content-Type': 'application/json',
     'app_key': configuracao.OxfordAuthentication.app_key,
     'app_id': configuracao.OxfordAuthentication.app_id
 }
 
-var options = {
+let options = {
     url: configuracao.OxfordAuthentication.app_url + configuracao.OxfordAuthentication.entries_path,
     method: 'GET',
     headers: headers
 }
 
-exports.consultar = (palavra) => new Promise((resolve) => {
-    options.url = configuracao.OxfordAuthentication.app_url + configuracao.OxfordAuthentication.entries_path + palavra;
+module.exports = {
+    consultar: (palavra) => new Promise((resolve) => {
+        options.url = configuracao.OxfordAuthentication.app_url + configuracao.OxfordAuthentication.entries_path + palavra;
+    
+        let pronunciations = [];
+        let definitions = [];
+        let examples = [];
+    
+        request(options, (error, res, body) => {
+            let json = JSON.parse(body);
+            let tempArr = [];
+    
+            let objetosArray = function (data, index) {
+                tempArr.push(data);
+            }
+    
+            json.results.forEach(objetosArray);
+            let lexicalEntries = [];
+    
+            tempArr.forEach((data, index) => {
+                lexicalEntries.push(data.lexicalEntries);
+            });
+    
+    
+            tempArr = [];
+    
+            lexicalEntries.forEach(objetosArray);
+    
+            tempArr[0].forEach((data, index) => {
+                pronunciations.push(data.pronunciations);
+            });
+    
+            tempArr = [];
+    
+            pronunciations = pronunciations[0];
+    
+            let entries = [];
+    
+            lexicalEntries.forEach((data, index) => {
+                entries.push(data);
+            });
+            tempArr = [];
+    
+            let senses = [];
+    
+            entries[0].forEach((data, index) => {
+                senses.push(data.entries);
+            });
+            tempArr = [];
+    
+            senses.forEach(objetosArray);
+    
+    
+            tempArr[0].forEach((data, index) => {
+                definitions.push(data.senses);
+            });
+    
+            tempArr = [];
+    
+            definitions.forEach(objetosArray);
+    
+    
+            tempArr[0].forEach((data, index) => {
+                examples.push(data.examples);
+            })
+    
+            tempArr = [];
+    
+            definitions = definitions[0];
+    
+            examples.forEach(objetosArray);
+    
+            examples = tempArr[0];
 
-    var pronunciations = [];
-    var definitions = [];
-    var examples = [];
-
-    request(options, (error, res, body) => {
-        var json = JSON.parse(body);
-        var tempArr = [];
-
-        var objetosArray = function (data, index) {
-            tempArr.push(data);
-        }
-
-        json.results.forEach(objetosArray);
-        var lexicalEntries = [];
-
-        tempArr.forEach((data, index) => {
-            lexicalEntries.push(data.lexicalEntries);
+            resolve({
+                pronunciations: pronunciations,
+                definitions: definitions,
+                examples: examples
+            })
+    
         });
-
-
-        tempArr = [];
-
-        lexicalEntries.forEach(objetosArray);
-
-        tempArr[0].forEach((data, index) => {
-            pronunciations.push(data.pronunciations);
-        });
-
-        tempArr = [];
-
-        pronunciations = pronunciations[0];
-
-        // pronunciations.forEach(element => {
-        //     console.log(element.phoneticSpelling);
-        // });
-        // console.log('-----------');
-
-        var entries = [];
-
-        lexicalEntries.forEach((data, index) => {
-            entries.push(data);
-        });
-        tempArr = [];
-
-        var senses = [];
-
-        entries[0].forEach((data, index) => {
-            senses.push(data.entries);
-        });
-        tempArr = [];
-
-        senses.forEach(objetosArray);
-
-
-        tempArr[0].forEach((data, index) => {
-            definitions.push(data.senses);
-        });
-
-        tempArr = [];
-
-        definitions.forEach(objetosArray);
-
-
-        tempArr[0].forEach((data, index) => {
-            examples.push(data.examples);
-        })
-
-        tempArr = [];
-
-        definitions = definitions[0];
-
-        // definitions.forEach(element => {
-        //     console.log(element.definitions[0]);
-        // });
-        // console.log('-----------');
-
-
-        examples.forEach(objetosArray);
-
-        examples = tempArr[0];
-
-        // examples.forEach(element => {
-        //     console.log(element.text);
-        // });
-        // console.log('-----------');
-
-        // console.log('--'+retorno.definitions);
-        resolve({
-            pronunciations: pronunciations,
-            definitions: definitions,
-            examples: examples
-        })
-
-    });
-});
+    }) 
+}
