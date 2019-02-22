a3e5e3e71f769c0ec208cba169e85a0bdbbb2099
Igor Carlos Almeida <almeidaigorcarlos@gmail.com>
2019-02-22 10:00:23 -0300

    Refatoracao do módulo 'ankiConnect.js'

--- Servicos/ankiconnect.js
+++ Servicos/ankiconnect.js
@@ -1,55 +1,55 @@
 const configuracao = require('./../Arquivos/configuracao');
 const request = require('request')
 
-exports.adicionar = (card) => {
-    let backPronuncia = "";
-    let backDefinicao = "DEFINITIONS <br/>";
-    let backExemplo = "EXAMPLES <br/>";
+module.exports = {
+    adicionar: (card) => new Promise((resolve, reject) => {
+        let backPronuncia = "";
+        let backDefinicao = "DEFINITIONS <br/>";
+        let backExemplo = "EXAMPLES <br/>";
 
-    card.pronuncia.forEach((data, index) => {
-        backPronuncia += `${data.phoneticSpelling} <br/>`
-    });
+        card.pronuncia.forEach((data, index) => {
+            backPronuncia += `${data.phoneticSpelling} <br/>`
+        });
 
-    card.definicao.forEach((data, index) => {
-        backDefinicao += `${index+1} - ${data.definitions} <br/>`
-    });
+        card.definicao.forEach((data, index) => {
+            backDefinicao += `${index + 1} - ${data.definitions} <br/>`
+        });
 
-    card.exemplos.forEach((data, index) => {
-        backExemplo += `${index+1} - ${data.text}<br/>`
-    });
+        card.exemplos.forEach((data, index) => {
+            backExemplo += `${index + 1} - ${data.text}<br/>`
+        });
 
-    this.request.post({
-        body: {
-            action: "addNote",
-            version: 6,
-            params: {
-                note: {
-                    deckName: this.configuracao.AnkiConnect.deck,
-                    modelName: "Basic",
-                    fields: {
-                        Front: card.frase,
-                        Back: `${backPronuncia}</br></br>${backDefinicao}</br></br><sub>${backExemplo}</sub>`
-                    },
-                    options: {
-                        allowDuplicate: true
-                    },
-                    tags: [
-                    ]
+        request.post({
+            body: {
+                action: "addNote",
+                version: 6,
+                params: {
+                    note: {
+                        deckName: configuracao.AnkiConnect.deck,
+                        modelName: "Basic",
+                        fields: {
+                            Front: card.frase,
+                            Back: `${backPronuncia}</br></br>${backDefinicao}</br></br><sub>${backExemplo}</sub>`
+                        },
+                        options: {
+                            allowDuplicate: true
+                        },
+                        tags: [
+                        ]
+                    }
                 }
-            }
-        },
-        url: this.configuracao.AnkiConnect.path,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Cache-Control': 'no-cache',
-            'Postman-Token': 'bb798423-2e03-4a1a-9463-f01ee6be8a06'
-        },
-        json: true
-    }, (err, res, body) => {
-        if (err) console.log(err);
-        else {
-            console.log(body)
-        }
-    });
+            },
+            url: configuracao.AnkiConnect.path,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache',
+                'Postman-Token': 'bb798423-2e03-4a1a-9463-f01ee6be8a06'
+            },
+            json: true
+        }, (err, res, body) => {
+            if(err) reject(false);
+            else resolve(true)
+        });
+    })
 }
--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const desserializador = require('./Servicos/desserializadorJSON');
-const configuracao = desserializador.recuperarArquivoConfiguracao();
-
 const listaFrases = require('./Servicos/listaFrases');
-const listaFrasesConteudo = listaFrases.retornarFrases(configuracao.ListaPath);
-
 const dicionario = require('./Servicos/dicionario');
-
 const ankiConnect = require('./Servicos/ankiconnect');
 
-let RegExp = /(?<=(!))(\w|\d|\n|[().,\-:;@#$%^&*\[\]"'+–/\/®°⁰!?{}|`~]| )+?(?=(!))/;
+const configuracao = desserializador.recuperarArquivoConfiguracao();
+const listaFrasesConteudo = listaFrases.retornarFrases(configuracao.ListaPath);
 
+const RegExp = /(?<=(!))(\w|\d|\n|[().,\-:;@#$%^&*\[\]"'+–/\/®°⁰!?{}|`~]| )+?(?=(!))/;
 let listaPalavras = [];
 
 listaFrasesConteudo.forEach((data, number, err) => {
     var palavra = RegExp.exec(data);
-    if (palavra != null) {
 
+    if (palavra != null) {
         dicionario.consultar(palavra[0]).then(element => {
             let card = {
                 frase: data,
@@ -23,15 +20,8 @@ listaFrasesConteudo.forEach((data, number, err) => {
                 definicao: element.definitions,
                 exemplos: element.examples
             }
-
-            try {
-                ankiConnect.adicionar(card);    
-            } catch (error) {
-                console.log('---'+error);
-            }
             
+            ankiConnect.adicionar(card);
         });
     }
 });
-
-console.log('rrr');
