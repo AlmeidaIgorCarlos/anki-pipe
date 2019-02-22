0a240c039fbfa10a73a2555c990c8f25fa9374d2
Igor Carlos Almeida <almeidaigorcarlos@gmail.com>
2019-02-22 09:46:20 -0300

    Refatoracao do modulo 'listaFrases'

--- Servicos/listaFrases.js
+++ Servicos/listaFrases.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
-exports.retornarFrases = function(path){
-    var lista = fs.readFileSync(path, {encoding:"UTF-8"}).split('\n');
-    return lista;
+module.exports = {
+    retornarFrases: (path)=>{
+        return fs.readFileSync(path, {encoding:"UTF-8"}).split('\n');   
+    }
 }
