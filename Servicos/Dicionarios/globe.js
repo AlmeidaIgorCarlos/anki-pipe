05d9c389fe8b55f503c4d64bf3c8d6ff5a047a80
logitao <gmdramos@gmail.com>
2019-02-24 17:50:11 -0300

    reafactor

--- Servicos/Dicionarios/globe.js
+++ Servicos/Dicionarios/globe.js
@@ -1,27 +1,50 @@
-const request = require('request');
+const request = require("request");
 
 module.exports = {
-
-    consultarDefinicao: (palavra) => {
-        return new Promise((resolve) => {
-            request(`https://glosbe.com/gapi/translate?from=eng&dest=eng&format=json&phrase=${palavra}&pretty=true`, (err, res, body) => {
-                if (err || res.statusCode == 404) return resolve([`não foi encontrado nenhuma definicao para a palavra: ${palavra}`]);
-                const json = JSON.parse(body);
-                return resolve(
-                    json.tuc[0].meanings.map(meaning => meaning.text)
-                )
-            })
-        });
-    },
-    consultarExemplo: (palavra) => {
-        return new Promise((resolve) => {
-            request(`https://glosbe.com/gapi/tm?from=eng&dest=eng&format=json&phrase=${palavra}&pretty=true`, (err, res, body) => {
-                if (err || res.statusCode == 404) return resolve([`não foi encontrado nenhum exemplo para a palavra: ${palavra}`]);
-                const json = JSON.parse(body);
-                return resolve(
-                    json.examples.map(example => example.first)
-                )
-            })
-        });
-    }
-}
+  consultarDefinicao: palavra => {
+    return new Promise(resolve => {
+      request(
+        `https://glosbe.com/gapi/translate?from=eng&dest=eng&format=json&phrase=${palavra}&pretty=true`,
+        (err, res, body) => {
+          if (err || res.statusCode !== 200) {
+            resolve([
+              `não foi encontrado nenhuma definicao para a palavra: ${palavra}`
+            ]);
+          } else if (res.statusCode === 200) {
+            const json = JSON.parse(body);
+            resolve(
+              json.tuc.length > 0
+                ? json.tuc[0].meanings.map(meaning => meaning.text)
+                : [
+                    `não foi encontrado nenhuma definicao para a palavra: ${palavra}`
+                  ]
+            );
+          }
+        }
+      );
+    });
+  },
+  consultarExemplo: palavra => {
+    return new Promise(resolve => {
+      request(
+        `https://glosbe.com/gapi/tm?from=eng&dest=eng&format=json&phrase=${palavra}&pretty=true`,
+        (err, res, body) => {
+          if (err || res.statusCode !== 200)
+            resolve([
+              `não foi encontrado nenhum exemplo para a palavra: ${palavra}`
+            ]);
+          else if (res.statusCode === 200) {
+            const json = JSON.parse(body);
+            resolve(
+              json.examples.length > 0
+                ? json.examples.map(example => example.first)
+                : [
+                    `não foi encontrado nenhuma definicao para a palavra: ${palavra}`
+                  ]
+            );
+          }
+        }
+      );
+    });
+  }
+};
--- Servicos/Dicionarios/oxford.js
+++ Servicos/Dicionarios/oxford.js
@@ -1,30 +1,39 @@
-const request = require('request');
-const configuracao = require('./../../Arquivos/configuracao');
+const request = require("request");
+const configuracao = require("./../../Arquivos/configuracao");
 
-const headers = {
-    'Content-Type': 'application/json',
-    'app_key': configuracao.OxfordAuthentication.app_key,
-    'app_id': configuracao.OxfordAuthentication.app_id
-}
-let options = {
-    url: configuracao.OxfordAuthentication.app_url + configuracao.OxfordAuthentication.entries_path,
-    method: 'GET',
-    headers: headers
-}
+const baseURL =
+  configuracao.OxfordAuthentication.app_url +
+  configuracao.OxfordAuthentication.entries_path;
+
+const options = {
+  url: baseURL,
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    app_key: configuracao.OxfordAuthentication.app_key,
+    app_id: configuracao.OxfordAuthentication.app_id
+  }
+};
 
 module.exports = {
-    consultarPronuncia: function (palavra) {
-        options.url = configuracao.OxfordAuthentication.app_url + configuracao.OxfordAuthentication.entries_path + palavra;
+  consultarPronuncia: function(palavra) {
+    options.url = baseURL + palavra;
 
-        return new Promise((resolve) => {
-            request(options, (err, res, body) => {
-                if (err || res.statusCode == 404) return resolve(['Nenhuma pronúncia encontrada']);
-                let json = JSON.parse(body);
+    return new Promise(resolve => {
+      request(options, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          resolve(["Nenhuma pronúncia encontrada"]);
+        }
+        if (res.statusCode === 200) {
+          let json = JSON.parse(body);
 
-                return resolve(
-                    json.results[0].lexicalEntries.map(lexicalEntry => lexicalEntry.pronunciations[0].phoneticSpelling)
-                )
-            });
-        });
-    }
-}
+          resolve(
+            json.results[0].lexicalEntries.map(
+              lexicalEntry => lexicalEntry.pronunciations[0].phoneticSpelling
+            )
+          );
+        }
+      });
+    });
+  }
+};
